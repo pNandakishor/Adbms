1eb28dba8142e10343dea8fb1f1e113beb787977
pNandakishor <144203008+pNandakishor@users.noreply.github.com>
2024-06-23 16:08:46 +0530

    Add files via upload

---
+++ alumnihome.php
@@ -0,0 +1,26 @@
+<?php
+    include './navigationbar.php';
+    session_start();
+    $_SESSION["logged_in"]=TRUE;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href=".assets/css/stylesheetalumni.css" />
+</head>
+<body>
+    <br><br>
+    <div class="alumnimain">
+        <div class="msg">
+            <h3>WELCOME</h3><br>
+            <p> TO THE <b>ALUMNI PORTAL</b> OF <b>SREE VISVESVARAYA INSTITUTE OF &nbspSCIENCE AND TECHNOLOGY !! </b>
+        </p></b><br>
+        <img src="./assets/images/college.lpeg.jfif" alt="img" height="520px" width=100%>
+        </div>        
+    </div>
+</body>
+</html>
---
+++ navbarindex.php
@@ -0,0 +1,50 @@
+<?php
+    include './dbconnect.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="./assets/css/stylehome.css" />
+    <title>ALUMNI PORTAL </title>
+  </head>
+  <body>
+    <div class="main">
+        <div class="topheader">
+            <div class="link">
+                <ul>
+                    <li>
+                        <b><a href="index.php" target="_self">HOME</a> &nbsp&nbsp&nbsp||</b>
+                    </li>
+                    <li>
+                        <b>&nbsp&nbsp<a href="alumnilogin.php" target="_self">ALUMNI LOGIN</a> &nbsp&nbsp&nbsp||</b>
+                    </li>
+                    <li>
+                        <b>&nbsp&nbsp<a href="adminlogin.php" target="_self">ADMIN LOGIN</a></b>
+                    </li>
+                </ul>
+        
+                
+                <!-- <p><b><a href="index.php" target="_self">HOME</a>&nbsp&nbsp&nbsp||&nbsp&nbsp&nbsp
+                <a href="alumnilogin.php" target="_self">ALUMNI LOGIN</a>&nbsp&nbsp&nbsp||&nbsp&nbsp&nbsp
+                <a href="adminlogin.php" target="_self">ADMIN LOGIN</a></b></p> -->
+            </div>
+            <!-- <div class="link">
+                <ul>
+                <p><b><a href="index.php" target="_self">HOME</a>&nbsp&nbsp&nbsp||&nbsp&nbsp&nbsp
+                <a href="alumnilogin.php" target="_self">ALUMNI LOGIN</a>&nbsp&nbsp&nbsp||&nbsp&nbsp&nbsp
+                <a href="adminlogin.php" target="_self">ADMIN LOGIN</a></b></p>
+                </ul>
+            </div> -->
+        </div>
+        <div class="header">
+            <div class="logo">
+                <a href="https://www.svitsmbnr.ac.in/"><img src="./assets/images/svitslogo.jpg" alt="logo"></a>                 
+                <div class="text"><b> &nbsp&nbsp&nbsp ALUMNI PORTAL</b></div>           
+            </div>
+        </div>
+</div>
+</body>
+</html>
